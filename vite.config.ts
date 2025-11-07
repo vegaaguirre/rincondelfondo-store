4a0042d75f1ecbb8b7e19bcf83f6743e51660dd5
Roberto Carlos Vega Aguirre <roberto.vega@interfactura.com>
2025-11-06 19:25:44 -0600

    Con esto subimos lo necesario para que genere el sitemap.

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import sourceIdentifierPlugin from 'vite-plugin-source-identifier'
-import sitemap from 'vite-plugin-sitemap'
+import sitemap from 'vite-plugin-sitemap' // <-- 1. IMPORTAR
 
 const isProd = process.env.BUILD_MODE === 'prod'
+
 export default defineConfig({
   plugins: [
     react(), 
@@ -14,8 +15,17 @@ export default defineConfig({
       includeProps: true,
     }),
     sitemap({
-      hostname: 'https://rincondelfondo.store/',
-    }),
+      hostname: 'https://rincondelfondo.store',
+      exclude: [
+        '/admin/login',
+        '/admin/dashboard',
+        '/admin/add-product',
+        '/admin/edit-product/*'
+      ],
+      dynamicRoutes: [
+        '/',
+      ]
+    } as any)
   ],
   resolve: {
     alias: {
